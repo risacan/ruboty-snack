fe2df9190de94b032198b825732400f1d07e78b0
Risa Harada <supikateriburu@gmail.com>
2016-10-05 23:04:09 +0900

    :seedling: Initial commit

---
+++ lib/ruboty/snack.rb
@@ -0,0 +1,7 @@
+require "ruboty/snack/version"
+
+module Ruboty
+  module Snack
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/snack/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Snack
+    VERSION = "0.1.0"
+  end
+end
