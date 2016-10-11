cee0a3af4a31469aa386313396b1c121c64175c4
Risa Harada <supikateriburu@gmail.com>
2016-10-11 23:34:37 +0900

    :white_check_mark: Add test!
    emoji is too cute

--- spec/ruboty/snack_spec.rb
+++ spec/ruboty/snack_spec.rb
@@ -5,3 +5,29 @@ describe Ruboty::Snack do
     expect(Ruboty::Snack::VERSION).not_to be nil
   end
 end
+
+describe Ruboty::Handlers::Snack do
+  let(:robot) do
+    Ruboty::Robot.new
+  end
+
+  let(:hand_with_snack) do
+    %w(
+      🌰 🍏 🍎 🍐 🍊 🍋 🍌 🍉 🍇 🍓 🍈 🍒 🍑 🍍 🍅 
+      🍆 🌽 🍠 🍞 🧀 🍗 🍖 🍤 🍳 🍟 🌭 🍕 🍝 🌮 🌯
+      🍜 🍲 🍥 🍣 🍱 🍛 🍙 🍚 🍘 🍢 🍡 🍧 🍨 🍦 🍰
+      🎂 🍮 🍬 🍭 🍫 🍿 🍩 🍪 🍄
+    ).map{ |snack| "つ#{snack}" }
+  end
+
+  describe "#feed_snack" do
+    it "gives a snack" do
+      expect(robot).to receive(:say).with(
+        hash_including(
+          body: satisfy { |v| hand_with_snack.include?(v) }
+        )
+      )
+      robot.receive(body: "#{robot.name} お腹すいた")
+    end
+  end
+end
