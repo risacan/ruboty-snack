ed51cd084f3d2bd62aa15e66bef99ceb84b7a3ae
Risa Harada <supikateriburu@gmail.com>
2016-10-11 21:32:51 +0900

    :fire:

--- spec/ruboty/snack_spec.rb
+++ spec/ruboty/snack_spec.rb
@@ -4,8 +4,4 @@ describe Ruboty::Snack do
   it "has a version number" do
     expect(Ruboty::Snack::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
